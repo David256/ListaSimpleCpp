0de32c9ca2a4a73fdc6d5b3ac1f222c013220cf8
David256 <davidborja456@gmail.com>
2017-03-03 13:50:15 -0500

    agregando el programa principal

---
+++ ElManipuladorDeListas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* Mmm... Manipulador Mmmm... */
+
+/*
+* Este código hace uso de la clase Lista
+* @author David256 (en github)
+*
+*/
+
+int main(int argc, char** argv) {
+	return 0;
+}
